0aa98fdfb29b819120c654ca8431b3714a4fba2c
kaique-barros <kaiqbarss@outlook.com>
2024-08-17 11:45:40 -0300

    Bases v2

---
+++ View/Animations/new_project.js
@@ -0,0 +1,6 @@
+let botao_novo_projeto = document.querySelector("#new_project");
+
+botao_novo_projeto.addEventListener("click", (e) => {
+    botao_novo_projeto.style.width = "100%"
+    botao_novo_projeto.style.heigth = "100%"
+})

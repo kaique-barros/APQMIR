058885f8e3acea1e06546aa3665a2695eb306bd3
kaique-barros <kaiqbarss@outlook.com>
2024-08-17 17:48:37 -0300

    RESET

--- View/Animations/new_project.js
+++
@@ -1,6 +0,0 @@
-let botao_novo_projeto = document.querySelector("#new_project");
-
-botao_novo_projeto.addEventListener("click", (e) => {
-    botao_novo_projeto.style.width = "100%"
-    botao_novo_projeto.style.heigth = "100%"
-})

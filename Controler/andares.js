304e0926fe98838b9b002228a64a9ca9f641a9d8
kaique-barros <kaiqbarss@outlook.com>
2024-09-20 20:54:01 -0300

    Bases 1.71

--- Controler/andares.js
+++ Controler/andares.js
@@ -1,12 +1,15 @@
 function adicionar_andar(predio){ 
-    $.ajax({
-        url:"../View/Containers/andar.html",
-        dataType: "html"})
-            .done((layout) => {
-            let nome_div_andares = predio + "-andares";
-            $("#"+predio+" > div")[0].id = nome_div_andares;
-            $("#" + nome_div_andares).prepend(layout);
-            atualizar_numero_andares(predio, nome_div_andares);
+    return new Promise((resolve) => {
+        $.ajax({
+            url:"../View/Containers/andar.html",
+            dataType: "html"})
+                .done((layout) => {
+                let nome_div_andares = predio + "-andares";
+                $("#"+predio+" > div")[0].id = nome_div_andares;
+                $("#" + nome_div_andares).prepend(layout);
+                atualizar_numero_andares(predio, nome_div_andares);
+                resolve(10);
+        })
     })
 };
 
@@ -15,8 +18,21 @@ function atualizar_numero_andares(predio, nome_div_andares){
     let andar = predio + "-andar_" + andares.length;
     $("#"+ nome_div_andares + " > .andar")[0].id = andar;
     $("#" + andar + " > h1")[0].innerHTML = "ANDAR " + andares.length;
+    $("#" + andar + " input[type=radio]")[0].name = predio + "_seq"
 }
 
-function atualizar_andares_tipo(predio) {
-    
+
+async function duplicar_andar(andar_base){
+    let predio = andar_base.slice(0,8);
+    await adicionar_andar(predio);
+    let numero_andar_novo = $("#" + predio + " .andar").length;
+    let andar_novo = predio + "-andar_" + numero_andar_novo;
+    let valores = {
+        tel_pts: $("#" + andar_base + " input")[0].value,
+        cftv_pts: $("#" + andar_base + " input")[1].value,
+        voip_pts: $("#" + andar_base + " input")[2].value
+    }
+    $("#" + andar_novo + " input")[0].value = valores.tel_pts;
+    $("#" + andar_novo + " input")[1].value = valores.cftv_pts;
+    $("#" + andar_novo + " input")[2].value = valores.voip_pts;
 }
--- Model/calcular.js
+++ Model/calcular.js
@@ -5,7 +5,7 @@ function calcular() {
         dist_max:0, 
         qnt_fibras:0
     }
-    let backbone_lvl_2_PP = {
+    let backbone_lvl_2 = {
         dist_interna:0, 
         velocidade:0, 
         qnt_fibras:0
@@ -20,18 +20,52 @@ function calcular() {
         voip_pts:0, 
         dist_media:0
     }
-    let tel_pts;
-    let cftv_pts;
-    let voip_pts;
+    let malha_horizontal_TT = {
+        tel_pts:0, 
+        cftv_pts:0, 
+        voip_pts:0, 
+        dist_media:0
+    }
+    
+    let predios = pega_predios();
+
+    predios.forEach(predio => {
+        backbone_lvl_1.dist_predios += predio.dist_ate_EF;
+        backbone_lvl_1.dist_max = backbone_lvl_1.dist_max < predio.dist_ate_EF ? predio.dist_ate_EF : backbone_lvl_1.dist_max // verifica se a distancia maxima é inferior a distancia do predio e troca se for
+
+        backbone_lvl_2.dist_interna 
+    });
+
+}
+
+function pega_predios() {
     let predios = [];
     
     for(let i = 0; i < $(".predio").length; i++){
-        let info = $(".predio")[i];
-        console.log(info);
+        let predio_atual = $(".predio")[i].id;
         let predio = {
-            id : info.id,
-            andares : null,
-            dist_ate_EF : $(id + " > input[name=dist_ate_ef")
+            id : predio_atual,
+            andares : pega_andares(predio_atual),
+            dist_ate_EF: $("#" + predio_atual + " input[name=dist_ate_ef]")[0].value,
+            is_ef: $("#" + predio_atual + " input[name=dist_ate_ef]")[0].value == 0 ? true : false 
         }
+        predios.push(predio)
     }
+    return predios;
+}
+
+function pega_andares(predio) {
+    let andares = [];
+    for(let i = $("#" + predio + " .andar").length - 1; i > -1; i--){
+        let andar_atual = $("#" + predio + " .andar")[i].id;
+        let andar = {
+            id: andar_atual.slice(9),
+            tel_pts: $("#" + andar_atual + " input[name=tel_pts")[0].value,
+            cftv_pts: $("#" + andar_atual + " input[name=cftv_pts")[0].value,
+            voip_pts: $("#" + andar_atual + " input[name=voip_pts")[0].value,
+            tem_seq: $("#" + andar_atual + " input[name="+ predio +"_seq")[0].checked
+        }
+        andares.push(andar);
+    }
+    return andares;
 }

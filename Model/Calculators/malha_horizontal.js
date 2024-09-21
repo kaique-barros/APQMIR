8f9549ac6447479ab8db6a38d57c1ffa174ba725
rafamendess <rafasilvam220@gmail.com>
2024-09-20 22:48:49 -0300

    Bases 1.72.3

---
+++ Model/Calculators/malha_horizontal.js
@@ -0,0 +1,106 @@
+let mhMateriais = [
+    {
+        nome: 'Tomada RJ 45 Fêmea',
+        unidade: "unid",
+        quantidade: 0
+    },
+    {
+        nome: 'Patch Cord Tamanho 3 m',
+        unidade: "unid",
+        quantidade: 0,
+        categoria: [{
+            tipo: '6',
+            cor: ['azul', 'verde', 'amarelho'],
+        },
+        {
+            tipo: '5e',
+            cor: ['azul', 'verde', 'amarelo']
+        }]
+    },
+    {
+        nome:'Espelho de conexão',
+        unidade:'unid',
+        quantidade:0,
+        categoria:['2x4','4x4']
+       },
+       
+
+    {
+        nome: 'Etiquetas para identificação de tomadas e espelho',
+        unidade: 'unid',
+        quantidade: 0
+    },
+
+    {
+        nome:'Cabo UTP  rígido',
+        unidade:'caixas',
+        quantidade:0,
+        categoria:['6','5e']
+    },
+    {
+        nome:'PPMH (Patch Panel de Malha Horizontal) 24P',
+        unidade:'unid',
+        quantidade:0
+    },
+    {
+        nome:'Organizador frontal de cabo - Tipo -  (Unidade de altura e largura: 19 polegadas)',
+        unidade:'unid',
+        quantidade:0
+    },
+    {
+        nome:'(Patch Cable) (Tamanho: 2m)',
+        unidade:'unid',
+        quantidade:0,
+        categoria:[
+            {
+                tipo: '6',
+                cor: ['azul', 'verde', 'amarelho'],
+            },
+            {
+                tipo: '5e',
+                cor: ['azul', 'verde', 'amarelo']
+            }
+        ]
+    },
+    {
+        nome:'Parafuso Porca Gaiola (conjunto com 10 unidades)',
+        unidade:'Conjuntos',
+        quantidade:0
+    },
+    {
+        nome:'Abraçadeira de velcro de 3m',
+        unidade:'metros',
+        quantidade:0
+    },
+    {
+        nome:'Abraçadeira Hellermann (conjunto com 100 unidades)',
+        unidade:'Conjunto',
+        quantidade:0
+    },
+    {
+        nome:'Filtro de linha com 06 tomadas',
+        unidade:'unid',
+        quantidade:0
+    },
+    {
+        nome:'Etiquetas para Patch Panel',
+        unidade:'unid',
+        quantidade:0
+    },
+    {
+        nome:'Etiquetas para portas do Patch Panel',
+        unidade:'unid',
+        quantidade:0
+    },
+    {
+        nome:'Etiquetas para identificação dos Patch Cables',
+        unidade:'unid',
+        quantidade:0
+    },
+    {
+        nome:'Etiqueta identificação do cabo de malha horizontal',
+        unidade:'unid',
+        quantidade:0
+    }
+
+];
